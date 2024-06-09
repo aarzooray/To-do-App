18e76fb18e07a80eef215e73d38d3647ee8ac5e3
Pankaj <techyray35@gmail.com>
2024-06-09 21:34:41 +0530

    All set to Launch

---
+++ logic.js
@@ -0,0 +1,107 @@
+
+var cont = document.querySelector('#container');
+
+var inputTask = document.querySelector('#inputTask');
+var inputBox = document.querySelector('.inputTaskBox');
+
+var addTask = document.querySelector('#addBtn');
+
+//
+function addTaskFun()
+{
+
+
+addTask.addEventListener("click", function () {
+    var taskBox = document.createElement('div');
+
+    taskBox.classList.add('taskBox');
+
+    var task = document.createElement('li');
+    task.innerHTML = inputTask.value;
+    task.classList.add('task');
+
+
+    var deleteBtn = document.createElement('button');
+    deleteBtn.classList.add('btn');
+    deleteBtn.id = "delBtn";
+    deleteBtn.innerHTML = `<i class="ri-delete-bin-line"></i>`;
+
+    var doneBtn = document.createElement('button');
+    doneBtn.classList.add('btn');
+    doneBtn.id ="doneBtn"
+    doneBtn.innerHTML = `<i class="ri-check-line"></i>`;
+
+    taskBox.appendChild(task);
+    taskBox.appendChild(deleteBtn);
+    taskBox.appendChild(doneBtn);
+    cont.appendChild(taskBox);
+
+
+    deleteBtn.addEventListener("click", function () {
+        taskBox.remove();
+    })
+
+    doneBtn.addEventListener("click", function () {
+        task
+        task.style.textDecoration = "line-through"
+        task.style.textDecorationColor = "red"
+    })
+
+    inputTask.value = ""
+})
+
+}
+
+function addTaskFunEnter(){
+    if(inputTask.value.trim() === "")
+        {
+    window.addEventListener("keydown", function (event) {
+        if(event.key == "Enter")
+            {
+                event.preventDefault();
+        var taskBox = document.createElement('div');
+    
+        taskBox.classList.add('taskBox');
+    
+        var task = document.createElement('li');
+        task.innerHTML = inputTask.value;
+        task.classList.add('task');
+    
+    
+        var deleteBtn = document.createElement('button');
+        deleteBtn.classList.add('btn');
+        deleteBtn.id ="delBtn"
+        deleteBtn.innerHTML = `<i class="ri-delete-bin-line"></i>`;
+    
+        var doneBtn = document.createElement('button');
+        doneBtn.classList.add('btn');
+        doneBtn.id ="doneBtn"
+        doneBtn.innerHTML = `<i class="ri-check-line"></i>`;
+    
+        taskBox.appendChild(task);
+        taskBox.appendChild(deleteBtn);
+        taskBox.appendChild(doneBtn);
+        cont.appendChild(taskBox);
+    
+    
+        deleteBtn.addEventListener("click", function () {
+            taskBox.remove();
+        })
+    
+        doneBtn.addEventListener("click", function () {
+            task.style.textDecoration = "line-through"
+            task.style.textDecorationColor = "red"
+        })
+    
+        inputTask.value = ""
+    }
+    })
+}
+else{
+    alert("hello")
+}
+}
+
+
+addTaskFunEnter();
+addTaskFun();
